b319f228ca43b8e25d09b16b199daf270781af5e
Elizabeth Witengier <ewitengier@ufl.edu>
2022-11-20 10:02:00 -0500

    rendering down code to basics to make sure that css and handlebars can be vviewed

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const app = require('./app')
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 3000
+const exphbs = require('express-handlebars')
 
-app.listen(PORT, function() {
+app.engine('handlebars', exphbs.engine())
+app.set('view engine','handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.get('/recipes', (req, res) => {
+  res.render('recipes')
+})
+
+app.get('/favorites', (req, res) => {
+  res.render('favorites')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.listen(PORT, () => {
   console.log('listening on http://localhost:' + PORT)
 })
 
