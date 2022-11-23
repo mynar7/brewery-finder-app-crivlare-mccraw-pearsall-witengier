eced83d0a24363cb2c028ff3459aa1e8171341c5
Elizabeth Witengier <ewitengier@ufl.edu>
2022-11-23 11:02:58 -0500

    basic site vision

--- index.js
+++ index.js
@@ -1,54 +1,33 @@
-const app = require('./app')
-const PORT = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const PORT = 3000
+const exphbs = require('express-handlebars')
 
-app.listen(PORT, function() {
-  console.log('listening on http://localhost:' + PORT)
+app.engine('handlebars', exphbs.engine())
+app.set('view engine','handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
-//added from other index.js
-/*
-const form = document.getElementById('brewery-form')
-const breweryURL = "https://api.openbrewerydb.org/breweries?by_postal="
-const beer = document.getElementById('beer')
-const search = document.getElementById('zip-search')
-const lnBreak = document.createElement('br')
+app.get('/search', (req, res) => {
+  res.render('search')
+})
 
-form.onsubmit = async e => {
-    e.preventDefault()
-    const searchTerm = search.value.trim()
-    console.log(searchTerm)
-    if(!searchTerm) return
+app.get('/recipes', (req, res) => {
+  res.render('recipes')
+})
 
-    try {
-        const res =  await fetch(`${breweryURL}${searchTerm}`) 
-        if (res.status !== 200) {
-            throw new Error("Location not found")
-        }
-        const breweryData = await res.json()
-        console.log(breweryData)
-        renderBreweries(breweryData, searchTerm)
-        beer.appendChild(lnBreak)
-        form.search.value = ""
-    }
-    catch (err){
-        form.search.value = ""
-        beer.innerHTML = err.message
-    }
-} 
+app.get('/favorites', (req, res) => {
+  res.render('favorites')
+})
 
-function renderBreweries(breweryData, searchTerm){
-    beer.innerHTML = `<h2>Breweries near ${searchTerm}: <br><br>`
-    for (let i = 0; i < breweryData.length; i++) {
-        let h3 = document.createElement('h3')
-        h3.textContent = breweryData[i].name
-        let p = document.createElement('p')
-        p.textContent = breweryData[i].street
-        beer.appendChild(h3)
-        beer.appendChild(p)
-        const line = document.createElement('hr')
-        line.setAttribute("width", "200")
-        beer.appendChild(line)
-    }
+app.get('/login', (req, res) => {
+  res.render('login')
+})
 
-}
-*/
+app.listen(PORT, () => {
+  console.log('listening on http://localhost:' + PORT)
+})
